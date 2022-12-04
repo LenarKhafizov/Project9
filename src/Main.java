36a5bfc08dbcd76210100e4836a5d1c1f2243a79
Lenar Khafizov <lenarkh@mail.ru>
2022-12-04 18:51:50 +0300

    Инициализация проекта к домашнему заданию по теме: Stream API. Потоки, повторные вызовы, основные методы.

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\t Домашнее задание по теме: " + "\"Stream API. Потоки, повторные вызовы, основные методы.\"");
+    }
+}
