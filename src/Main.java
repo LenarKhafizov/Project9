911c9f1cf7ca91588580e13e29aaf89c834c100a
Lenar Khafizov <lenarkh@mail.ru>
2022-12-07 21:26:42 +0300

    Подсчет количества несовершеннолетних реализован.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("\t Домашнее задание по теме: " + "\"Stream API. Потоки, повторные вызовы, основные методы.\"");
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        Stream <Person>  stream1 = persons.stream();
+        System.out.println("Количество несовершеннолетних: " + stream1.filter(person -> person.getAge() < 18).count());
     }
 }
